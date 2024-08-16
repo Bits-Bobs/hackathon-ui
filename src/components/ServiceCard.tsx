cff56a637ccfb4c90de65694a3a28ed3ee2bc78a
Fiantso Harena <fiantsorav@gmail.com>
2024-08-16 15:16:44 +0300

    feat: service section

---
+++ src/components/ServiceCard.tsx
@@ -0,0 +1,31 @@
+import AOS from 'aos';
+import { FC, useEffect } from 'react';
+
+type ServiceCard = {
+  item: {
+    id: number;
+    title: string;
+    description: string;
+    img: string;
+  };
+};
+
+export const ServiceCard: FC<ServiceCard> = ({ item }) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <article>
+      <div className='relative h-[344px] w-[252px] cursor-pointer rounded-lg text-white'>
+        <img
+          src={item.img}
+          alt='Item image'
+          className='h-full w-full rounded-lg object-cover saturate-0 filter transition-all duration-500 ease-in-out hover:saturate-100'
+        />
+        <span className='absolute bottom-3 left-3 text-xs font-normal'>{item.description}</span>
+        <span className='absolute bottom-10 left-4 text-[20px] font-semibold'>{item.title}</span>
+      </div>
+    </article>
+  );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,5 @@ export * from './Footer';
 export * from './Navbar';
 export * from './Button';
 export * from './Hero';
+export * from './ServiceSlide';
+export * from './ServiceCard';
