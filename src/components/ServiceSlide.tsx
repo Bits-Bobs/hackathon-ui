06129b1ead6bdc47902fb3d6285ffa31c24213c7
Fiantso Harena <fiantsorav@gmail.com>
2024-08-16 17:14:21 +0300

    style: center services card

--- src/components/ServiceSlide.tsx
+++ src/components/ServiceSlide.tsx
@@ -6,7 +6,7 @@ type ServiceSlideProps = {
 
 export const ServiceSlide: FC<ServiceSlideProps> = ({ children }) => {
   return (
-    <section className='justify-center py-12' data-aos='fade-down'>
+    <section className='flex justify-between py-12' data-aos='fade-down'>
       <div className='mx-auto flex max-w-[1080px] flex-row gap-5'>{children}</div>
     </section>
   );
