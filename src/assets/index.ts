01d2a2ff036109ddcbe602080983c1b6aa4f4e3b
Fiantso Harena <fiantsorav@gmail.com>
2024-08-16 16:14:10 +0300

    fix: outlet problem

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,12 @@
+import Accessibility from './accessibility.png';
 import BannerImg from './bannerImage.webp';
+import Coin from './coin.png';
+import Facebook from './facebook.png';
+import Instagram from './instagram.png';
+import Language from './language.png';
+import LinkedIn from './linkedin.png';
+import Pinterest from './pinterest.png';
 import ProfileAvatar from './profile.jpg';
 import Service from './service.jpg';
 
-export { ProfileAvatar, BannerImg, Service };
+export { ProfileAvatar, BannerImg, Service, Accessibility, Coin, Facebook, Instagram, Language, LinkedIn, Pinterest };
--- src/layouts/LandingLayout.tsx
+++ src/layouts/LandingLayout.tsx
@@ -1,12 +1,21 @@
-import { Outlet } from 'react-router-dom';
-
-import { Footer, Navbar } from '../components';
+import { Footer, Hero, Navbar, ServiceCard, ServiceSlide } from '../components';
+import { services } from '../lib';
 
 export const LandingLayout = () => {
   return (
     <main className='font-poppins'>
       <Navbar />
-      <Outlet />
+      <section>
+        <Hero />
+        <div className='py-10'>
+          <h1 className='text-center text-4xl font-semibold text-[#100C0D]'>Our services</h1>
+          <ServiceSlide>
+            {services.map((service) => (
+              <ServiceCard item={service} key={service.id} />
+            ))}
+          </ServiceSlide>
+        </div>
+      </section>
       <Footer />
     </main>
   );
