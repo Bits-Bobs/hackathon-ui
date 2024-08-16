c18b16119ee0fc9d1f30f4730489ca468ab1e446
fedoraAnjara <hei.fedora.3@gmail.com>
2024-08-16 18:15:22 +0300

    feat: finalizes service card content

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -17,12 +17,12 @@ export const services = [
     id: 2,
     title: 'Recycling',
     description: ' Une opportunités pour un avenir plus vert',
-    img: Service,
+    img: 'https://www.forwardsingapore.gov.sg/-/media/esc/featuredconversations/mobile/mccy4_masthead_mobile_e_360x543.jpg?h=543&w=360&hash=34BE191155BF2CCC15C6086D590E1053715CD1ED29A2F0027F18CF328AF9A499',
   },
   {
     id: 3,
-    title: 'Service 3',
-    description: 'Some description',
-    img: Service,
+    title: 'sale recycled articles',
+    description: 'Discover our recycled products: elegant, durable and environmentally friendly.',
+    img: 'https://img.freepik.com/photos-premium/sac-femme-fleches-recyclage-studio-pour-emballage-vente-au-detail-durable-fond-bleu-produit-ecologique-changement-climatique-fille-africaine-responsable-sauver-planete_590464-203153.jpg',
   },
 ];
