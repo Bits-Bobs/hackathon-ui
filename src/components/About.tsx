86d68a17201f1a6ee5a4bed5079165c35d4f8e65
Fiantso Harena <fiantsorav@gmail.com>
2024-08-16 18:58:45 +0300

    style: capitalize title

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,9 +7,9 @@ export const About = () => {
           <div className='text-[14px] text-gray-700' style={{ flex: 1 }} data-aos='fade-down'>
             At HackaFako, we believe in the power of technology to transform lives, protect the environment, and
             strengthen communities. Our mission is to harness the potential of digital innovation to create solutions
-            that serve both people and the planet. Inspired by the theme <i>"Le numérique au service du vivant, des
-            communautés et de l’environnement,"</i> Hackafako is a platform that connects like-minded individuals passionate
-            about using technology to address real-world challenges.
+            that serve both people and the planet. Inspired by the theme{' '}
+            <i>"Le numérique au service du vivant, des communautés et de l’environnement,"</i> Hackafako is a platform
+            that connects like-minded individuals passionate about using technology to address real-world challenges.
             <br /> <br />
             Our team is dedicated to developing tools and applications that promote sustainability, enhance community
             well-being, and contribute to the preservation of our natural ecosystems. We strive to bridge the gap
