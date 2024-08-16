dc2d70f29fa6ee7a7e8763e500412d4ceb73417f
Fiantso Harena <fiantsorav@gmail.com>
2024-08-16 18:41:20 +0300

    style: scroll behavior smooth

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ export const About = () => {
       <div className='mx-auto max-w-[1080px]'>
         <h1 className='mb-20 text-center text-4xl font-semibold text-[#100C0D]'>About us</h1>
         <div className='flex w-full justify-between gap-x-5'>
-          <div className='text-[14px] text-gray-700' style={{ flex: 1 }}>
+          <div className='text-[14px] text-gray-700' style={{ flex: 1 }} data-aos='fade-down'>
             At HackaFako, we believe in the power of technology to transform lives, protect the environment, and
             strengthen communities. Our mission is to harness the potential of digital innovation to create solutions
             that serve both people and the planet. Inspired by the theme <i>"Le numérique au service du vivant, des
@@ -17,7 +17,7 @@ export const About = () => {
             and environmental stewardship. Join us on this journey as we use the power of code to make a positive impact
             on the world, one solution at a time.
           </div>
-          <div className='right' style={{ flex: 1 }}>
+          <div className='right' style={{ flex: 1 }} data-aos='fade-down'>
             <img
               src={
                 'https://www.prospektphoto.net/wp-content/uploads/2021/01/prospekt-city-of-flies-riccardo-bononi-irfoss-imp-festival-internazionale-fotogiornalismo-padova-scaled.jpg'
