477da87d3cf71eb1f912368be618d95138eab94d
Fiantso Harena <fiantsorav@gmail.com>
2024-08-16 18:56:31 +0300

    style: our services section fix images

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-import { Facebook, Instagram, LinkedIn, Pinterest, Service, Twitter } from '../assets';
+import { Facebook, Instagram, LinkedIn, Pinterest, Service1, Service2, Service3, Twitter } from '../assets';
 import { PATH_DASHBOARD } from '../routes/path';
 
 export const currentUser = {
@@ -12,19 +12,19 @@ export const services = [
     id: 1,
     title: 'Waste Pickup',
     description: 'contribute to a healthier environment and encourage a sustainable future.',
-    img: 'https://bucketlistjourney.net/wp-content/uploads/2023/12/Organize-a-Community-Clean-Up-RF-2-768x576.jpg',
+    img: Service1,
   },
   {
     id: 2,
     title: 'Recycling',
     description: ' Une opportunités pour un avenir plus vert',
-    img: 'https://www.forwardsingapore.gov.sg/-/media/esc/featuredconversations/mobile/mccy4_masthead_mobile_e_360x543.jpg?h=543&w=360&hash=34BE191155BF2CCC15C6086D590E1053715CD1ED29A2F0027F18CF328AF9A499',
+    img: Service2,
   },
   {
     id: 3,
     title: 'sale recycled articles',
     description: 'Discover our recycled products: elegant, durable and environmentally friendly.',
-    img: 'https://img.freepik.com/photos-premium/sac-femme-fleches-recyclage-studio-pour-emballage-vente-au-detail-durable-fond-bleu-produit-ecologique-changement-climatique-fille-africaine-responsable-sauver-planete_590464-203153.jpg',
+    img: Service3,
   },
 ];
 
